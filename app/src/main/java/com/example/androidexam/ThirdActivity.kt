a29c7eb9dcc5a30f8a38afba3068840aa2c22f7b
LaurentBaj <laurent_baj@hotmail.com>
2021-05-04 18:45:17 +0200

    Layout Prototype finished

--- app/src/main/java/com/example/androidexam/ThirdActivity.kt
+++ app/src/main/java/com/example/androidexam/ThirdActivity.kt
@@ -1,11 +1,18 @@
 package com.example.androidexam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        findViewById<TextView>(R.id.toTransactions).setOnClickListener {
+            val intent = Intent(this, SeventhActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
