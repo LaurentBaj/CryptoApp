7cd66d7ed27039cd3e0c3f4673d11faa8c906983
LaurentBaj <laurent_baj@hotmail.com>
2021-05-05 02:59:10 +0200

    Activity 5 and 6 get passed data

--- app/src/main/java/com/example/androidexam/SixthActivity.kt
+++ app/src/main/java/com/example/androidexam/SixthActivity.kt
@@ -2,10 +2,25 @@ package com.example.androidexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidexam.databinding.ActivityFifthBinding
+import com.example.androidexam.databinding.ActivitySixthBinding
+import com.squareup.picasso.Picasso
 
 class SixthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySixthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sixth)
+        binding = ActivitySixthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intentPassed = intent.getStringArrayListExtra("data")
+        val data = IntentData(intentPassed!![0], intentPassed[1], intentPassed[2])
+        Picasso.get()
+                .load("https://static.coincap.io/assets/icons/${data.symbol}@2x.png")
+                .into(binding.sixthImage)
+
+        binding.sixthHeaderName.text = data.name
+        binding.sixthHeaderPrice.text = data.priceUsd
     }
 }
