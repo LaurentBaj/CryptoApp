935b6b436a1c0a33a0a8d7b530f55a6ac561a040
LaurentBaj <laurent_baj@hotmail.com>
2021-05-06 17:57:31 +0200

    Third Act also gets points

--- app/src/main/java/com/example/androidexam/ThirdActivity.kt
+++ app/src/main/java/com/example/androidexam/ThirdActivity.kt
@@ -1,16 +1,39 @@
 package com.example.androidexam
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.androidexam.databinding.ActivityThirdBinding
+import com.example.androidexam.viewmodel.ThirdViewModel
 
 class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    private val viewModel = ThirdViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_third)
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<TextView>(R.id.toTransactions).setOnClickListener {
+        viewModel.dbInit(this)
+
+        var sharedPreferences: SharedPreferences = getSharedPreferences("com.example.androidexam.preference", Context.MODE_PRIVATE)
+
+        viewModel.Points.observe(this, {
+            binding.thirdPoints.text = "Points: $it"
+        })
+
+        // Check new user
+        if(sharedPreferences.getLong("FIRST_STARTUP_ID", -1L) == -1L) {
+            viewModel.atInstall()
+            sharedPreferences.edit().putLong("FIRST_STARTUP_ID", 1L).apply()
+        }
+
+
+        binding.toTransactions.setOnClickListener {
             val intent = Intent(this, SeventhActivity::class.java)
             startActivity(intent)
         }
